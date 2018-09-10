e3d0a0858c0adb26ce177fb38ac36985abaaed1b
yamanaka <yamanaka@meme.ne.jp>
2018-09-10 20:29:41 +0900

    mailcheckerのイメージ追加

--- html/index.php
+++ html/index.php
@@ -2,8 +2,13 @@
 require_once __DIR__.'/../vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem('./view');
 $twig = new Twig_Environment($loader);
+$test = "bbbb";
+echo date("Y/m/d H:i:s");
 echo $twig->render('hello.html', array('name' => 'Twig'));
 phpinfo();
+/*mb_language("Japanese");
+mb_internal_encoding("UTF-8");
+mb_send_mail('yamanaka@meme.ne.jp', 'テスト送信テスト送信', "テスト送信テスト送信テスト送信テスト送信");*/
 
 ?>
 
--- index.php
+++ index.php
@@ -5,5 +5,6 @@
  * Date: 2018/04/11
  * Time: 11:56
  */
-
+//composer 起動方法
+//docker-compose run composer -v
 phpinfo();
