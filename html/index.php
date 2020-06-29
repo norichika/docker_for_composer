02d194bf1c648fe35784a8c506a8b1611456bbf2
yamanaka <yamanaka@meme.ne.jp>
2020-06-29 13:34:48 +0900

    PHPのサーバー設定、DB設定の更新

--- html/index.php
+++ html/index.php
@@ -6,9 +6,7 @@ $test = "bbbb";
 echo date("Y/m/d H:i:s");
 echo $twig->render('hello.html', array('name' => 'Twig'));
 phpinfo();
-/*mb_language("Japanese");
+mb_language("Japanese");
 mb_internal_encoding("UTF-8");
-mb_send_mail('yamanaka@meme.ne.jp', 'テスト送信テスト送信', "テスト送信テスト送信テスト送信テスト送信");*/
-
-?>
+//mb_send_mail('yamanaka@meme.ne.jp', 'テスト送信テスト送信', "テスト送信テスト送信テスト送信テスト送信");
 
